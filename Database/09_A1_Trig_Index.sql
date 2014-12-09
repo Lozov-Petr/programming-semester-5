26ce2346ff389d4b453599392b625f60f877f42c
Peter <not_kill@mail.ru>
2014-12-09 21:51:27 +0400

    Update 9-10 tasks

--- Database/09_A1_Trig_Index.sql
+++ Database/09_A1_Trig_Index.sql
@@ -2,8 +2,8 @@
 -- Проверка корректности рейса (взлетает раньше, чем садися и не летит туда, откуза вылетел)
 CREATE TRIGGER ValidationFlight ON LP_Flight INSTEAD OF INSERT
 AS 
-	IF EXISTS(SELECT COUNT(*) FROM INSERTED WHERE TimeOfDeparture > TimeOfArrival OR PlaceOfDeparture = PlaceOfArrival)
-		ROLLBACK;
+    IF EXISTS(SELECT COUNT(*) FROM INSERTED WHERE TimeOfDeparture > TimeOfArrival OR PlaceOfDeparture = PlaceOfArrival)
+        ROLLBACK;
 
 GO
 
@@ -11,17 +11,17 @@ GO
 CREATE TRIGGER ValidationSeats ON LP_Order FOR INSERT
 AS
 
-	IF EXISTS(SELECT *
+    IF EXISTS(SELECT *
               FROM INSERTED 
               JOIN LP_Flight ON INSERTED.FlightID = LP_Flight.FlightID
               JOIN LP_Aircraft ON LP_Flight.AircraftID = LP_Aircraft.AircraftID
               WHERE NOT NumberOfSeats BETWEEN 1 AND CountOfVacantSeats) 
-	   OR
-	   EXISTS(SELECT *
-	          FROM INSERTED
+       OR
+       EXISTS(SELECT *
+              FROM INSERTED
               JOIN LP_Order ON INSERTED.FlightID = LP_Order.FlightID 
                            AND INSERTED.NumberOfSeats = LP_Order.NumberOfSeats
-			               AND INSERTED.OrderID != LP_Order.OrderID)
+                           AND INSERTED.OrderID != LP_Order.OrderID)
     ROLLBACK;
 
 GO
--- Database/10_A1_Proc.sql
+++ Database/10_A1_Proc.sql
@@ -1,21 +1,21 @@
--- Подарить самому расточительному клиенту билет
+﻿-- Подарить самому расточительному клиенту билет
 CREATE PROCEDURE PresentTicketToTheBestClient
-	@FlightNumber VARCHAR(30),   -- Дарим билет на этот рейс
-	@NumberOfSeats INTEGER,      -- На это место
-	@NewOrderNumber VARCHAR(30)  -- Номер,который получит подарочный билет
+    @FlightNumber VARCHAR(30),   -- Дарим билет на этот рейс
+    @NumberOfSeats INTEGER,      -- На это место
+    @NewOrderNumber VARCHAR(30)  -- Номер,который получит подарочный билет
 AS
-	-- Находим ID самого расточительного клиента
-	DECLARE @BestClientID INTEGER = (SELECT TOP(1) ClientID FROM
-										(SELECT LP_Client.ClientID, SUM(Price) AS SumPrice FROM LP_Client
-									     JOIN LP_Order ON LP_Client.ClientID = LP_Order.ClientID
-									     GROUP BY LP_Client.ClientID) AS T
-								     ORDER BY SumPrice DESC);
+    -- Находим ID самого расточительного клиента
+    DECLARE @BestClientID INTEGER = (SELECT TOP(1) ClientID FROM
+                                        (SELECT LP_Client.ClientID, SUM(Price) AS SumPrice FROM LP_Client
+                                         JOIN LP_Order ON LP_Client.ClientID = LP_Order.ClientID
+                                         GROUP BY LP_Client.ClientID) AS T
+                                     ORDER BY SumPrice DESC);
     -- Находим ID рейса
-	DECLARE @FlightID INTEGER = (SELECT FlightID FROM LP_Flight WHERE Number = @FlightNumber);
+    DECLARE @FlightID INTEGER = (SELECT FlightID FROM LP_Flight WHERE Number = @FlightNumber);
 
-	-- Создаем билет (к созданию заказа должен быть прикреплён триггер, оповещающий клиента о билете) 
-	INSERT INTO LP_Order(FlightID, ClientID, Number, Price,	NumberOfSeats)
-		VALUES(@FlightID, @BestClientID, @NewOrderNumber, 0, @NumberOfSeats);
+    -- Создаем билет (к созданию заказа должен быть прикреплён триггер, оповещающий клиента о билете) 
+    INSERT INTO LP_Order(FlightID, ClientID, Number, Price,    NumberOfSeats)
+        VALUES(@FlightID, @BestClientID, @NewOrderNumber, 0, @NumberOfSeats);
 
 GO
 
