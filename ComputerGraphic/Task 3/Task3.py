e1574fc1c964d1a459e8e64ac911ecf553adf4e0
Peter <not_kill@mail.ru>
2015-01-23 15:55:10 +0400

    Update task 3

--- ComputerGraphic/Task 3/Task3.py
+++ ComputerGraphic/Task 3/Task3.py
@@ -1,4 +1,5 @@
 from cv2 import *
+from numpy import * 
 
 img = imread("text.bmp")
 
@@ -9,17 +10,30 @@ img = Laplacian(img, IPL_DEPTH_32F, 3)
 
 _, img = threshold(img, 50, 255, THRESH_BINARY)
 
-img = dilate(img, getStructuringElement(MORPH_RECT, (3, 3), (1, 1)))
-img = erode(img, getStructuringElement(MORPH_RECT, (3, 3), (1, 1)))
-img = dilate(img, getStructuringElement(MORPH_RECT, (3, 3), (1, 1)))
+structuringElement = getStructuringElement(MORPH_RECT, (5, 4))
+
+img = dilate(img, structuringElement)
+img = erode(img, structuringElement)
 
 img = cvtColor(img, COLOR_BGR2GRAY)
 
-contours, _ = findContours(img, RETR_EXTERNAL, CHAIN_APPROX_NONE)
+h, w = img.shape
+mask = zeros((h+2, w+2), uint8)
 
-for contour in contours:
-    left, top, width, height = boundingRect(contour)
-    rectangle(realImg, (left, top), (left + width, top + height), (0,0,255))
+imshow("Internal", img)
+
+for i in range(0,w) :
+	for j in range(0,h) :
+		if img[j,i] == 255 :
+		 	_, (left, top, width, height) = floodFill(img, mask, (i,j), 0)
+		 	rectangle(realImg, (left, top - 1), (left + width, top + height - 1), (0,0,255))
+
+
+# contours, _ = findContours(img, RETR_EXTERNAL, CHAIN_APPROX_NONE)
+
+#for contour in contours:
+#    left, top, width, height = boundingRect(contour)
+#    rectangle(realImg, (left, top), (left + width, top + height), (0,0,255))
 
 
 imshow("Result", realImg)
